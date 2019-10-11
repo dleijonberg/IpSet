ec1ba12e5fdb7b810950c3b903398c77c47ef151
Daniel Leijonberg <dleijonberg@gmail.com>
2019-10-11 11:07:19 +0200

    Added exitsing solution

---
+++ NetworkAdapterTestConsole/Nics.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.NetworkInformation;
+
+namespace NetworkAdapter
+{
+    class Nics
+    {
+        private NetworkInterface[] _adapters;
+        
+        public NetworkInterface[] Adapters 
+        {
+            get 
+            {
+                _adapters = NetworkInterface.GetAllNetworkInterfaces();
+                return _adapters;
+            }
+        }
+        public struct NicInfo
+        {
+            public string Name;
+            public string ID;
+            public string[] IpAddress;
+            public string[] Ipv4Mask;
+        }
+
+        public List<NicInfo> NicList = new List<NicInfo>();
+
+        public void UpdateNics()
+        {
+            var a = Adapters;
+            NicInfo n = new NicInfo();
+            int i = 0;
+            NicList.Clear();
+
+            foreach (var nic in a)
+            {
+                n.Name = nic.Name;
+                n.ID = nic.Id;
+                NicList.Add(n);
+            }
+        }
+
+        public NicInfo GetNicInfo(string NicName)
+        {
+            NicInfo n = new NicInfo();
+            UpdateNics();
+            var a = Adapters;
+
+            foreach (var nic in a)
+            {
+                if (NicName == nic.Name)
+                {
+                    var ipAddresses = nic.GetIPProperties().UnicastAddresses;
+                    for (int i = 0; i < ipAddresses.Count; i++)
+                    {
+                        if (!ipAddresses[i].Address.IsIPv6LinkLocal && !ipAddresses[i].Address.IsIPv6SiteLocal)
+                        {
+                            n.Ipv4Mask[i] = ipAddresses[i].IPv4Mask.ToString();
+                            n.IpAddress[i] = ipAddresses[i].Address.ToString();
+                        }
+                    }
+                }
+            }
+            return n;
+        }
+
+
+    }
+}
---
+++ NetworkAdapterTestConsole/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using NetworkAdapter;
+
+namespace NetworkAdapterTestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Nics NicClass = new Nics();
+            NicClass.UpdateNics();
+            // Nics.NicInfo CurrentAdapter;
+
+            for(int i = 0; i < NicClass.NicList.Count; i++)
+            {
+                Console.WriteLine("{0}: {1}", i, NicClass.NicList[i].Name);
+                //Console.WriteLine("   {0}", NicClass.NicList[i].ID);
+                //Console.WriteLine();
+            }
+            Console.Write("Which adapter? ");
+            string input = Console.ReadLine();
+            int num = Int32.Parse(input);
+
+            NicClass.GetNicInfo(NicClass.NicList[num].Name);
+        }
+    }
+}
