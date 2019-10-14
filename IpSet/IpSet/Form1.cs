bc322b4d0036e156ca62683ff379ddf24631de07
Daniel Leijonberg <dleijonberg@gmail.com>
2019-10-14 15:39:33 +0200

    Added subnet mask

--- IpSet/IpSet/Form1.cs
+++ IpSet/IpSet/Form1.cs
@@ -37,7 +37,7 @@ namespace IpSet
 
             };
 
-            InitTextBox();
+            InitGlobalControls();
 
             // Add form controls
             Controls.Add(cbNics);
@@ -64,22 +64,31 @@ namespace IpSet
         }
 
         internal TextBox tbIpAddress = new TextBox();
+        internal TextBox tbSubnetMask = new TextBox();
         internal Nics NicObject = new Nics();
 
-        private void InitTextBox()
+        private void InitGlobalControls()
         {
             this.tbIpAddress.Name = "tbIpAddress";
             this.tbIpAddress.Location = new System.Drawing.Point(300, 20);
             this.tbIpAddress.Width = 100;
+            //this.tbIpAddress.Height = 60;
+            //this.tbIpAddress.Multiline = true;
             this.tbIpAddress.ReadOnly = true;
-            this.Controls.Add(tbIpAddress);
+            this.Controls.Add(this.tbIpAddress);
 
+            this.tbSubnetMask.Name = "tbSubnetMask";
+            this.tbSubnetMask.Location = new System.Drawing.Point(300, 42);
+            this.tbSubnetMask.Width = 100;
+            this.tbSubnetMask.ReadOnly = true;
+            this.Controls.Add(this.tbSubnetMask);
         }
         public void cbNics_SelectedIndexChanged(object sender, System.EventArgs e)
         {
             ComboBox cbSender = (ComboBox) sender;
             this.tbIpAddress.Text = NicObject.NicList[cbSender.SelectedIndex].IpAddress;
-            
+            this.tbSubnetMask.Text = NicObject.NicList[cbSender.SelectedIndex].Ipv4Mask;
+
         }
     }
 }
